714589642b25ec29e052ffd486a6a140e2c737b3
Mateus Henrique <mhenriqq32@gmail.com>
2022-01-24 11:32:47 -0300

    modificaçoes efeito scroll

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
   const botaoProximo = document.querySelector(".carrossel__proximo");
   const botaoVoltar = document.querySelector(".carrossel__voltar");
   const listaCarrossel = document.querySelector(".carrossel__lista");
+  const itemCarrossel = document.querySelector(".carrossel__item");
 
   botaoProximo.addEventListener("click", () => {
-    listaCarrossel.scrollLeft += listaCarrossel.offsetWidth;
+    listaCarrossel.scrollLeft +=
+      listaCarrossel.offsetWidth - itemCarrossel.offsetWidth;
   });
 
   botaoVoltar.addEventListener("click", () => {
-    listaCarrossel.scrollLeft -= listaCarrossel.offsetWidth;
+    listaCarrossel.scrollLeft -=
+      listaCarrossel.offsetWidth - itemCarrossel.offsetWidth;
   });
 })();
